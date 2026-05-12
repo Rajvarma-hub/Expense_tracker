7a16c5ad81bea259c2db350e296f7d0b99f62833
Raj6301597150 <Rajvarma6301597150@gmail.com>
2026-05-12 14:04:44 +0530

    Added the output.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -203,19 +203,3 @@ resource "aws_eks_node_group" "main" {
     Name = "expense-node-group"
   }
 }
-
-# -----------------------------
-# OUTPUTS
-# -----------------------------
-
-output "backend_ecr_url" {
-  value = aws_ecr_repository.backend.repository_url
-}
-
-output "frontend_ecr_url" {
-  value = aws_ecr_repository.frontend.repository_url
-}
-
-output "cluster_name" {
-  value = aws_eks_cluster.main.name
-}
